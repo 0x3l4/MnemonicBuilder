b4e96edfc9997ab9756dcebc5f438206af5ef534
Alexander Vereta <alexander.git@yandex.ru>
2025-05-24 17:19:30 +0500

    Add controller and view index for sentence

---
+++ MnemonicBuilder/Controllers/SentenceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MnemonicBuilder.Controllers
+{
+    public class SentenceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
